59e1bf7637b0a5b9195e0670e2eb024f59e9d1a3
Vitor Borgatte <vitor.borgatte@icloud.com>
2022-11-25 10:43:36 -0300

    final dia 4
    adicionando função que permite que o usuário pesquise filmes através do input

--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
 import config from '../config.json' assert { type: 'json' };
 
 const mainContent = document.querySelector('main');
+var favorites = document.querySelectorAll('.like');
+const input = document.querySelector('.search');
 
 /* REQUISIÇÃO A TMDB API */
 /* https://api.themoviedb.org/3/ */
@@ -8,11 +10,10 @@ async function getPopularMovies() {
   const url = `https://api.themoviedb.org/3/discover/movie/?api_key=${config.apikey}&language=pt-BR`;
   const response = await fetch(url);
   const obj = await response.json();
-  return obj.results;
+  const final = obj.results.filter((obj) => obj.overview != '');
+  return final;
 }
 const popularMovies = await getPopularMovies();
-
-/* https://image.tmdb.org/t/p/original${}, title, vote_average, overview */
 
 /* RENDERIZAR FILME DINAMICAMENTE NO HTML */
 function renderMovie(movie) {
@@ -41,16 +42,34 @@ function renderMovie(movie) {
   card.innerHTML = markup;
 
   mainContent.appendChild(card);
+  favorites = document.querySelectorAll('.like');
 }
 
 popularMovies.forEach((movie) => renderMovie(movie));
+
+/* PESQUISA DE FILMES */
+async function searchMovie(value) {
+  const url = `https://api.themoviedb.org/3/search/movie?api_key=${config.apikey}&language=pt-BR-en-US&query=${value}`;
+  const response = await fetch(url);
+  const obj = await response.json();
+  const final = obj.results.filter((obj) => obj.overview != '');
+  return final;
+}
+
+async function searchMovieAfterKeyPress(event) {
+  if (event.key == 'Enter' && input.value != '') {
+    const searchedMovies = await searchMovie(input.value);
+    mainContent.innerHTML = '';
+    searchedMovies.forEach((movie) => renderMovie(movie));
+  }
+}
+
+input.addEventListener('keypress', searchMovieAfterKeyPress);
 
 /* FAVORITAR OU DESFAVORITAR FILME */
 function handleFavorite({ target }) {
   target.classList.toggle('active');
 }
-
-const favorites = document.querySelectorAll('.like');
 favorites.forEach((favorite) =>
   favorite.addEventListener('click', handleFavorite)
 );
